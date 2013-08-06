4ee5558d63b3a4891750e97cd0b5b41d0e1f851c
afrisch87 <afjstark@gmail.com>
2013-08-05 21:07:18 -0400

    add array examples

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+  //an example of a regular array
+  $myarray = array('steve','joe','stan');
+  $myarray[] = 'asher';
+  print_r($myarray);
+
+  $associative_array = array('name1' => 'joe', 'name2' =>'stan');
+  echo count($associative_array);
+  print_r ($associative_array);
+?>
